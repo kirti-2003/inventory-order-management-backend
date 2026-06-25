fd00d65df8329ed6eb4a7c7986aed56afeefa928
kirti-2003 <kirti.anand903@gmail.com>
2026-06-25 14:49:54 +0530

    resolve the CORS issue

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ origins = [
     "http://127.0.0.1:5173",
     "http://localhost:5174",
     "http://127.0.0.1:5174",
+    "https://inventory-order-management-frontend-liard.vercel.app",
 ]
 
 app.add_middleware(
